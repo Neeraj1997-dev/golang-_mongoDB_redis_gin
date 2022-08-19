d982b1ca62dc101ff7f32cb731973cacd6b48ea6
Kapable <tech@kapable.club>
2022-08-19 07:00:54 +0530

    redis connection

--- main.go
+++ main.go
@@ -1,37 +1,66 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 
-	"encoding/json"
+	"backend/config"
 
-	"github.com/go-redis/redis"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 )
 
-type Student struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+var (
+	server      *gin.Engine
+	ctx         context.Context
+	redisclient *redis.Client
+)
+
+func init() {
+
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Could not load environment variables", err)
+	}
+	ctx = context.TODO()
+	// Connect to Redis
+	redisclient = redis.NewClient(&redis.Options{
+		Addr: config.RedisUri,
+	})
+
+	if _, err := redisclient.Ping(ctx).Result(); err != nil {
+		panic(err)
+	}
+
+	err = redisclient.Set(ctx, "test", "Welcome to Golang with Redis", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Redis client connected successfully...")
+
+	//Create the Gin Engine instance
+	server = gin.Default()
 }
 
 func main() {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
+	config, err := config.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("Could not load config", err)
+	}
+	value, err := redisclient.Get(ctx, "test").Result()
+	if err == redis.Nil {
+		fmt.Println("key: test does not exist")
+	} else if err != nil {
+		panic(err)
+	}
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": value})
 	})
 
-	json, err := json.Marshal(Student{Name: "Neeraj Yadav", Age: 26})
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = client.Set("7541", json, 0).Err()
-	if err != nil {
-		fmt.Println(err)
-	}
-	val, err := client.Get("7541").Result()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(val)
+	log.Fatal(server.Run(":" + config.Port))
 }
